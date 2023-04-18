b98eeb6aa393624aac185df0d1ced39494b084fe
DogGod <lex982746297@163.com>
2023-04-18 10:32:07 +0800

    add sql

---
+++ sql/User_T_Data/dbo.r_ItemGrade.Table.sql
@@ -0,0 +1,8 @@
+USE [Rappelz_User]
+GO
+INSERT [dbo].[r_ItemGrade] ([grade], [_name], [nameKey], [colorCode]) VALUES (1, N'일반', N'IGRADE_NAME_1', N'#dedede')
+INSERT [dbo].[r_ItemGrade] ([grade], [_name], [nameKey], [colorCode]) VALUES (2, N'고급', N'IGRADE_NAME_2', N'#398e3d')
+INSERT [dbo].[r_ItemGrade] ([grade], [_name], [nameKey], [colorCode]) VALUES (3, N'마법', N'IGRADE_NAME_3', N'#2879ff')
+INSERT [dbo].[r_ItemGrade] ([grade], [_name], [nameKey], [colorCode]) VALUES (4, N'희귀', N'IGRADE_NAME_4', N'#7e57c2')
+INSERT [dbo].[r_ItemGrade] ([grade], [_name], [nameKey], [colorCode]) VALUES (5, N'전설', N'IGRADE_NAME_5', N'#fe9400')
+GO
